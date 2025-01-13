cbd7ad29901719b142b30b455cd381e59e54561f
ramaFE <suryarama4@gmail.com>
2025-01-14 00:45:51 +0700

    update

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -17,7 +17,7 @@
             @else
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Surya Rama</span>
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ Auth::user()->name }}</span>
                 <img class="img-profile rounded-circle"
                     src="{{ asset('undraw_profile.svg') }}">
             </a>
